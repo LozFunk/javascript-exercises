411675515ba050a6b99d831bcbda1d93dd9f421f
Alexandros Iosifidis <196984481+LozFunk@users.noreply.github.com>
2025-05-14 21:44:56 +0200

    Did the excerises form 06 - 11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
+  if (b > a) {
+    const temp = a;
+    a = b;
+    b = temp;
+  }
+
+    let sum = 0
+    for (let i = a; i <= b; i++){
+        sum += i;
+    }
+   return sum
 
 };
 
