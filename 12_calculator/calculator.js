d8ff205bcc31671c2aee75ce372beaf560d2b221
Alexandros Iosifidis <196984481+LozFunk@users.noreply.github.com>
2025-05-21 21:51:46 +0200

    Did some of the array excercises

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current)=> total + current,0)
+  };
+
+const multiply = function(array) {
+  return array.reduce((product, current)=> product * current)
 };
 
-const multiply = function() {
-
+const power = function(x,y) {
+	return Math.pow(x,y)
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(x) {
+  let product = 1;
+  for (let i = x; i > 0; i--){
+    product *= i
+  }
+  return product 
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+    const backwardStr = x.split('').reverse().join('').toLowerCase()
+    return backwardStr === x
 };
 
 // Do not edit below this line
